131657a356d56c16edae5e8ac92c20872fd2f108
Khanh Nguyen <kate.nguyen@siliconstack.com.au>
2023-05-10 00:30:51 +0700

    Updates

---
+++ precache-manifest.6ee21d26eb3fe42fce73af06a9b5e134.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31d9b06c4f20fb867cef8f1feb13a03a",
+    "url": "/emoji-search/index.html"
+  },
+  {
+    "revision": "c765162a3fcefb550ab2",
+    "url": "/emoji-search/static/css/2.5510c45e.chunk.css"
+  },
+  {
+    "revision": "c1d20e8fd26e89178796",
+    "url": "/emoji-search/static/css/main.f38963d9.chunk.css"
+  },
+  {
+    "revision": "c765162a3fcefb550ab2",
+    "url": "/emoji-search/static/js/2.120a0f45.chunk.js"
+  },
+  {
+    "revision": "6120b983a03909b7d2cd9bf134169811",
+    "url": "/emoji-search/static/js/2.120a0f45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1d20e8fd26e89178796",
+    "url": "/emoji-search/static/js/main.2a270629.chunk.js"
+  },
+  {
+    "revision": "53f65d824d8bd704bdf5",
+    "url": "/emoji-search/static/js/runtime-main.fcefcbdb.js"
+  }
+]);
